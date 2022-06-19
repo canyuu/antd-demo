ca8c3f48bbc098838ccd1cb0e7cdfb05647bf925
licanyu <295938285@qq.com>
2022-06-19 10:39:23 +0800

    向事件处理程序传递参数

--- src/docs/ReactEvent.js
+++ src/docs/ReactEvent.js
@@ -3,24 +3,22 @@ import React, {Component} from 'react';
 class ListItem extends Component {
   constructor(props) {
     super(props); // 子类中调用父类构造函数
-    // 方法2
-    this.handleClick=this.handleClick.bind(this);
+    this.state={
+      count: 1,
+    };
   }
-  doSomething() {
 
-  }
-  // 方法3，推荐使用，箭头函数的this总是指向定义时的对象，而不是运行时
-  handleClick=(e)=>{
+  handleClick=(id, e)=>{
+    console.log(id);
     console.log(e);
-    this.doSomething(); // 报错，会说找不到这个方法
   };
+
   render() {
     return (
       <div>
-        {/* 方法1*/}
-        <button onClick={this.handleClick.bind(this)}>
-            header
-        </button>
+        <button onClick={this.handleClick.bind(this, 1)}>header1</button>
+        <button onClick={()=>this.handleClick(2)}>header2</button>
+        <button onClick={(e)=>this.handleClick(3, e)}>header3</button>
       </div>
     );
   }
