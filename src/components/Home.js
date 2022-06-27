07e9cb454b0622e340e2b4ec17a9f7ae7c786f98
licanyu <295938285@qq.com>
2022-06-27 17:17:00 +0800

    新增路由相关组件，并工程化管理

---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const Home = () => {
+  return <>
+    首页
+  </>;
+};
+export default Home;
---
+++ src/components/User/Profile.js
@@ -0,0 +1,7 @@
+import React from 'react';
+function Profile() {
+  return (
+    <div>UserAdd</div>
+  );
+}
+export default Profile;
